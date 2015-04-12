c10ad9f1f73a76c8f3d13b1cec4fb85d86cca9e4
Ben Kahle <ben@kahle.org>
2015-04-12 13:27:23 -0400

    add hello world boilerplate.

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+var mongoURI = process.env.MONGOURI || "mongodb://localhost/slac";
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+mongoose.connect(mongoURI);
+
+app.listen(PORT, function() {
+  console.log("Application running on port:", PORT);
+});
