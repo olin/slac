6292eddce7e8bb495d4ac46cb5793e2145c4743a
Ben Kahle <ben@kahle.org>
2015-04-13 13:26:28 -0400

    add project route boilerplate.

---
+++ routes/project.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var express = require('express');
+
+var project = express.Router();
+
+project.get('/') = function(req, res) {
+
+};
+
+project.get('/:id') = function(req, res) {
+
+};
+
+project.post('/:id') = function(req, res) {
+
+};
+
+project.put('/:id') = function(req, res) {
+
+};
+
+project.delete('/:id') = function(req, res) {
+
+};
+
+module.exports = project;
