13ed4277c40aaf24638641d587fd406799f41bbe
Chris Lee <sihrc.c.lee@gmail.com>
2015-04-13 14:09:37 -0400

    FIX: Change keys to model keys for projects :panda_face:

--- models/projects.js
+++ models/projects.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.Schema({
-    "creator" : { type: mongoose.Schema.Types.ObjectId, ref: 'users' }
-  , "dateCreated" : Number    // Timestamp milliseconds long
+    "dateCreated" : Number    // Timestamp milliseconds long
   , "title" : String
   , "coverPhoto" : String     // Image Url
   , "members" : [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }]
@@ -10,5 +9,5 @@ module.exports = mongoose.Schema({
   , "organizations" : [{type: mongoose.Schema.Types.ObjectId,
                          ref: 'organizations'}]
   , "goals" : String
-    , "type" : String
+  , "type" : String
 });
